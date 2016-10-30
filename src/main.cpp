c5ba91356f4998d85243c0cf04b4805a1bface40
domagojb <boros.domagoj@gmail.com>
2016-10-30 19:29:31 +0100

    Initial commit; Create CMake config; Add Hello world template

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * Domagoj Boros
+ * Mateja Skriljak
+ * Ivan Jurin
+ *
+ * Bioinformatika, 2016.
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
