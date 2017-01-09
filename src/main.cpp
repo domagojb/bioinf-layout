b1da84b445a71a709d9b8ac16384effbed07d903
Ivan Jurin <ivan.jurin@mircoblink.com>
2017-01-09 17:26:06 +0100

    refactor main

--- src/main.cpp
+++ src/main.cpp
@@ -12,41 +12,6 @@
 
 #define DATASET "ecoli"
 
-int count(const Graph & g){
-    int cnt =0;
-    for ( const auto & pair : g ) {
-        for ( const auto & item : pair.second ) {
-            if(!item.del){
-                cnt++;
-            }
-        }
-    }
-    return cnt;
-}
-
-bool deleteShort( Graph g, ReadTrims readTrims, float r ) {
-    int n_short = 0;
-    for(auto & pair : g){
-        int max_ml = 0;
-
-        if(pair.second.size()<2) continue;
-
-        for ( auto & edge : pair.second ) {
-            max_ml = std::max(max_ml,edge.numberOfSequenceMatches);
-        }
-        if(max_ml != pair.second[0].numberOfSequenceMatches) continue;
-        int thres = pair.second[0].numberOfSequenceMatches * r + .499;
-        int i;
-        for (i = pair.second.size() - 1; i >= 1 && pair.second[i].numberOfSequenceMatches< thres; --i);
-        for (i = i + 1; i <  pair.second.size(); ++i)
-            pair.second[i].del = 1, ++n_short;
-    }
-
-    cleanGraph(g);
-    fprintf(stderr, "[M::%s] removed %d short overlaps\n", __func__, n_short);
-    return n_short!=0;
-}
-
 int main() {
 
     Overlaps overlaps;
@@ -89,20 +54,7 @@ int main() {
     std::cout << "11) Popping bubbles" << std::endl;
     popBubbles( g, readTrims );
 
-    for (int i = 0; i <= params.n_rounds; ++i) {
-        float r = params.min_ovlp_drop_ratio + (params.max_ovlp_drop_ratio - params.min_ovlp_drop_ratio) / params.n_rounds * i;
-
-        if(deleteShort( g, readTrims, r )) {
-            cutTips( g, readTrims, params );
-            popBubbles( g, readTrims );
-        }
-    }
-
     writeGraphToSIF( "../test-data/" DATASET "_nobubles.sif", g );
-//
-//    logGraph(g,readTrims);
-//    exit(0);
-
 
     Unitigs unitigs;
     std::cout << "12) Generating unitigs" << std::endl;
@@ -111,21 +63,6 @@ int main() {
     assignSequencesToUnitigs( unitigs, readTrims, "../test-data/" DATASET "_reads.fasta" );
 
     logUnitigs( unitigs, readTrims );
-
-
-    //    logGraph(g);
-
-    //    std::cout << "Left with " << overlaps.size() << " trimmed overlaps" << std::endl;
-
-    //
-    //    for (auto const &readTrim: readTrims) {
-    //        std::cout << readTrim.first << " " << readTrim.second.toString() << std::endl;
-    //    }
-
-
-
-    //    writeOverlapsToSIF("mnebijemte.sif", overlaps);
-
 
     return 0;
 }
